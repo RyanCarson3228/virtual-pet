7b9799aea4bb37506cb36575e3b50664e0f832ec
RyanCarson3228 <rcarson95@gmail.com>
2018-01-28 11:49:36 -0500

    After testing Boredom methods

--- src/main/java/virtualpet/VirtualPet.java
+++ src/main/java/virtualpet/VirtualPet.java
@@ -2,18 +2,16 @@ package virtualpet;
 
 public class VirtualPet {
 
-	int initialHunger;
-	int initialThirst;
-	int currentHunger = 15;
-	int currentThirst = 15;
+	public int initialHunger;
+	public int initialThirst;
+	public int currentHunger = 15;
+	public int currentThirst = 15;
+	public int currentBoredom = 20;
 
 	public VirtualPet() {
 	}
 
-	public VirtualPet(int initialHunger) {
-	}
-
-	public VirtualPet(int currentThirst, int currentHunger) {
+	public VirtualPet(int currentThirst, int currentHunger, int intialBoredom) {
 
 	}
 
@@ -25,6 +23,10 @@ public class VirtualPet {
 		return currentThirst -= 5;
 	}
 
+	public int play() {
+		return currentBoredom -= 5;
+	}
+
 	public int getHunger() {
 		return currentHunger;
 	}
@@ -33,9 +35,13 @@ public class VirtualPet {
 		return currentThirst;
 	}
 
+	public int getBoredom() {
+		return currentBoredom;
+	}
+
 	public void tick() {
 		currentHunger += 2;
 		currentThirst += 2;
+		currentBoredom += 2;
 	}
-
 }
--- src/test/java/virtualpet/VirtualPetTest.java
+++ src/test/java/virtualpet/VirtualPetTest.java
@@ -9,7 +9,7 @@ public class VirtualPetTest {
 	public void shouldBeLessHungryAfterFeeding() {
 		// arrange
 		int initialHunger = 20;
-		VirtualPet underTest = new VirtualPet(initialHunger, 0);
+		VirtualPet underTest = new VirtualPet(initialHunger, 0, 0);
 		// act
 		underTest.feed();
 		// assert
@@ -20,7 +20,7 @@ public class VirtualPetTest {
 	@Test
 	public void shouldBeMoreHungryAfterTick() {
 		// arrange
-		VirtualPet underTest = new VirtualPet(15, 0);
+		VirtualPet underTest = new VirtualPet(15, 0, 0);
 		// act
 		underTest.tick();
 		// assess
@@ -32,7 +32,7 @@ public class VirtualPetTest {
 	public void shouldBeLessThirstyAfterDrinking() {
 		// arrange
 		int initialThirst = 20;
-		VirtualPet underTest = new VirtualPet(0, initialThirst);
+		VirtualPet underTest = new VirtualPet(0, initialThirst, 0);
 		// act
 		underTest.drink();
 		// assess
@@ -43,11 +43,35 @@ public class VirtualPetTest {
 	@Test
 	public void shouldBeMoreThirstyAfterTick() {
 		// arrange
-		VirtualPet underTest = new VirtualPet(0, 15);
+		VirtualPet underTest = new VirtualPet(0, 15, 0);
 		// act
 		underTest.tick();
 		// assess
 		int currentThirst = underTest.getThirst();
-		Assert.assertEquals("Thirst should have increased by 2", 18, currentThirst);
+		Assert.assertEquals(17, currentThirst);
 	}
+
+	@Test
+	public void shouldBeLessBoredAfterPlay() {
+		// arrange
+		int intialBoredom = 20;
+		VirtualPet underTest = new VirtualPet(0, 0, intialBoredom);
+		// act
+		underTest.play();
+		// assess
+		int currentBoredom = underTest.getBoredom();
+		Assert.assertEquals(15, currentBoredom);
+	}
+
+	@Test
+	public void shouldBeMoreBoredAfterTick() {
+		// arrange
+		VirtualPet underTest = new VirtualPet(0, 0, 20);
+		// act
+		underTest.tick();
+		// assess
+		int currentBoredom = underTest.getBoredom();
+		Assert.assertEquals(22, currentBoredom);
+	}
+
 }
